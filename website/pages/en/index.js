adb4eff36cd3941a7a56c8bb93e10efda403d28d
Patrick Smith <patrick@burntcaramel.com>
2019-03-02 01:13:15 +1100

    Init with landing page

---
+++ website/pages/en/index.js
@@ -0,0 +1,293 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require("react");
+
+const CompLibrary = require("../../core/CompLibrary.js");
+
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+class HomeSplash extends React.Component {
+  render() {
+    const { siteConfig, language = "" } = this.props;
+    const { baseUrl, docsUrl } = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
+    const langPart = `${language ? `${language}/` : ""}`;
+    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+    const SplashContainer = props => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Logo = props => (
+      <div className="projectLogo">
+        <img src={props.img_src} alt="Project Logo" />
+      </div>
+    );
+
+    const ProjectTitle = () => (
+      <h2 className="projectTitle">
+        {siteConfig.title}
+        <small>{siteConfig.tagline}</small>
+      </h2>
+    );
+
+    const PromoSection = props => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Button = props => (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={props.href} target={props.target}>
+          {props.children}
+        </a>
+      </div>
+    );
+
+    return (
+      <SplashContainer>
+        <Logo img_src={`${baseUrl}img/collected-logo.svg`} />
+        <div className="inner">
+          <ProjectTitle siteConfig={siteConfig} />
+          <PromoSection>
+            <Button href="#try">Try It Out</Button>
+            <Button href={docUrl("doc1.html")}>Example Link</Button>
+            <Button href={docUrl("doc2.html")}>Example Link 2</Button>
+          </PromoSection>
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    const { config: siteConfig, language = "" } = this.props;
+    const { baseUrl } = siteConfig;
+
+    const Block = props => (
+      <Container
+        padding={["bottom", "top"]}
+        id={props.id}
+        background={props.background}
+      >
+        <GridBlock
+          align={props.align || "left"}
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
+    const FeatureCallout = () => (
+      <div
+        className="productShowcaseSection paddingBottom"
+        style={{ textAlign: "center" }}
+      >
+        <h2>Feature Callout</h2>
+        <MarkdownBlock>{`
+- Use content from GitHub repos
+- Read from Figma documents
+        `}</MarkdownBlock>
+      </div>
+    );
+
+    const QueryWithGraphQL = () => (
+      <Block id="query">
+        {[
+          {
+            title: "Query your existing content with GraqhQL",
+            content: [
+              "One place to query from GitHub, Trello, and Figma — all at once. Use SHAs to speed up with caching.",
+              "```graphql\n" + `
+query {
+  gitHubRepoSource(
+    owner: "RoyalIcing",
+    repoName: "collected.guide",
+    branch: "8cb73f4d3ebebe14c0eb49ffc5369817032567a2"
+  ) {
+    home: textMarkdown(path: "content/README.md") {
+      text
+      toHTML {
+        html
+      }
+    }
+  }
+
+  trelloBoardSource(boardID: "6bi5Hyct") {
+    lists {
+      name
+      cards {
+        content {
+          text
+        }
+      }
+    }
+  }
+}
+`.trim() + "\n```"
+            ].join("\n\n"),
+            image: `${baseUrl}img/collected-logo.svg`,
+            imageAlign: "right",
+          }
+        ]}
+      </Block>
+    );
+
+    const TryOut = () => (
+      <Block id="try">
+        {[
+          {
+            title: "Try it Out",
+            content: [
+              "You can get started with Collected in any JavaScript project. We have specific packages for React and Vue too.",
+              [
+                "- React: `npm add @collected/react`",
+                "- Vue: `npm add @collected/vue`",
+                "- Core: `npm add @collected/core`",
+              ].join("\n"),
+              "```jsx\n" + `
+import React from "react";
+import { useCollectedQL } from "@collected/react";
+import ReactMarkdown from "react-markdown";
+
+function Home() {
+  const { source: { home } } = useCollectedQL(\`
+{
+  source: gitHubRepoSource(
+    owner: "RoyalIcing",
+    repoName: "collected.guide",
+    branch: "8cb73f4d3ebebe14c0eb49ffc5369817032567a2"
+  ) {
+    home: textMarkdown(path: "content/README.md") {
+      text
+    }
+  }
+}
+\`);
+
+  return <article>
+    <ReactMarkdown source={home.text} />
+  </article>;
+}
+`.trim() + "\n```"
+            ].join("\n\n"),
+            image: `${baseUrl}img/collected-logo.svg`,
+            imageAlign: "right",
+          }
+        ]}
+      </Block>
+    );
+
+    const Description = () => (
+      <Block background="dark">
+        {[
+          {
+            content:
+              "This is another description of how this project is useful",
+            image: `${baseUrl}img/collected-logo.svg`,
+            imageAlign: "right",
+            title: "Description"
+          }
+        ]}
+      </Block>
+    );
+
+    const LearnHow = () => (
+      <Block background="light">
+        {[
+          {
+            title: "Learn How",
+            content: [
+              "You can build dynamic sites on Cloudflare Workers, dynamic sites on Node.js, and static sites.",
+              "You can prototype with content sourced from Trello. This lets you build something in minutes, plus the content can be managed by anyone with access to the Trello board.",
+              "You can manage content in GitHub, and then load it on-the-fly via jsDelivr, a multi-provider CDN. Point your app to a new commit sha, and it will update live, without having to redeploy. This means you can have rapidly changing content — and server render it too.",
+              "If a static website is all you need, then Collected also supports workflows that build to static HTML.",
+            ].join("\n\n"),
+            image: `${baseUrl}img/collected-logo.svg`,
+            imageAlign: "left"
+          }
+        ]}
+      </Block>
+    );
+
+    const Features = () => (
+      <Block layout="fourColumn" align="center">
+        {[
+          {
+            title: "Query with GraphQL",
+            content: "Source content from GitHub, Figma, or Trello.",
+            image: `${baseUrl}img/collected-logo.svg`,
+            imageAlign: "top"
+          },
+          {
+            title: "Process on the fly",
+            content: "Process Markdown, images, and records.",
+            image: `${baseUrl}img/collected-logo.svg`,
+            imageAlign: "top"
+          }
+        ]}
+      </Block>
+    );
+
+    const Showcase = () => {
+      if ((siteConfig.users || []).length === 0) {
+        return null;
+      }
+
+      const showcase = siteConfig.users
+        .filter(user => user.pinned)
+        .map(user => (
+          <a href={user.infoLink} key={user.infoLink}>
+            <img src={user.image} alt={user.caption} title={user.caption} />
+          </a>
+        ));
+
+      const pageUrl = page => baseUrl + (language ? `${language}/` : "") + page;
+
+      return (
+        <div className="productShowcaseSection paddingBottom">
+          <h2>Who is Using This?</h2>
+          <p>This project is used by all these people</p>
+          <div className="logos">{showcase}</div>
+          <div className="more-users">
+            <a className="button" href={pageUrl("users.html")}>
+              More {siteConfig.title} Users
+            </a>
+          </div>
+        </div>
+      );
+    };
+
+    return (
+      <div>
+        <HomeSplash siteConfig={siteConfig} language={language} />
+        <div className="mainContainer">
+          <Features />
+          {/* <FeatureCallout /> */}
+          <QueryWithGraphQL />
+          <TryOut />
+          <LearnHow />
+          {/* <Description /> */}
+          {/* <Showcase /> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
---
+++ website/siteConfig.js
@@ -0,0 +1,99 @@
+// See https://docusaurus.io/docs/site-config for all the possible
+// site configuration options.
+
+// List of projects/orgs using your project for the users page.
+const users = [
+  {
+    caption: 'Patrick Smith',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
+    image: '/img/collected-logo.svg',
+    infoLink: 'https://github.com/BurntCaramel',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'Collected', // Title for your website.
+  tagline: 'Query, process, and manage content',
+  url: 'https://collected.systems', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'collected',
+  organizationName: 'RoyalIcing',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'doc1', label: 'Docs'},
+    {doc: 'doc4', label: 'API'},
+    {page: 'help', label: 'Help'},
+    {blog: true, label: 'Blog'},
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  headerIcon: 'img/collected-logo.svg',
+  footerIcon: 'img/collected-logo.svg',
+  favicon: 'img/favicon.png',
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#d43960',
+    secondaryColor: '#b8154a',
+  },
+
+  /* Custom fonts for website */
+  /*
+  fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },
+  */
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} Patrick Smith`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: [/*'https://buttons.github.io/buttons.js'*/],
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: 'img/collected-logo.png',
+  twitterImage: 'img/collected-logo.png',
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
